dcd3529080560fe7f07c3cb9cc7b316446d789d9
Fabrix <fabricio.corvera4@gmail.com>
2024-10-29 21:56:26 -0600

    [Update] environment variables

--- backend/src/firebase.js
+++ backend/src/firebase.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { getStorage} from "firebase-admin/storage";
+import { getStorage } from "firebase-admin/storage";
 import { readFileSync } from "fs"; // Para leer el archivo de credenciales
 import dotenv from "dotenv";
 
@@ -9,14 +9,27 @@ const app = express();
 // Cargar variables de entorno
 dotenv.config();
 // Leer el archivo de credenciales JSON
-const serviceAccount = JSON.parse(
-  readFileSync(process.env.GCLOUD_PROJECT, "utf8") // Usa la variable de entorno
-);
+/* const serviceAccount = JSON.parse(
+  readFileSync(process.env.GCLOUD_PROJECT) // Usa la variable de entorno
+); */
+const firebaseConfig = {
+  type: process.env.TYPE,
+  project_id: process.env.PROYECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
 // Inicializar Firebase con credenciales
 initializeApp({
-  credential: cert(serviceAccount),
-  storageBucket: process.env.STORAGE_BUCKET // Usa la variable de entorno
+  credential: cert(firebaseConfig),
+  storageBucket: process.env.STORAGE_BUCKET, // Usa la variable de entorno
 });
 
 const db = getFirestore();
